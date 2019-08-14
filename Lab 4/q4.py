05db2b231d12e0907c3fea6b590ebd26fda96e5d
Anudit Nagar <nagaranudit@gmail.com>
2019-08-14 14:46:36 +0530

    Lab 4

--- Lab 4/q4.py
+++ Lab 4/q4.py
@@ -1,14 +1,28 @@
+from math import factorial as fact
+def nCr(n , r):
+    if (r==0):
+        return 1
+    if (r==1):
+        return n
+    return float(fact(n) / (fact(r)*fact(n-r)))
+
 TOTAL_TOSSES = 100
 TOTAL_CASES = 2 ** TOTAL_TOSSES
 
 print("====Q1====")
-print(2**70 / TOTAL_CASES)
-print("====Q2====")
-probSum = float(0)
-for i in range(36, 101):
-    prob = float((2**i) / TOTAL_CASES)
-    print(prob)
-    probSum = probSum + prob
-    print(probSum)
+print(float(nCr(TOTAL_TOSSES, 30) * ((1/2)**TOTAL_TOSSES)))
 
-# print(probSum)
+print("====Q2====")
+probSum = 1
+for i in range(1, 36):
+    probSum -= (nCr(TOTAL_TOSSES, i) * ((1/2)**TOTAL_TOSSES))
+print(probSum)
+
+print("====Q3====")
+probSum = 1
+for i in range(1, 60):
+    probSum -= (nCr(TOTAL_TOSSES, i) * ((1/2)**TOTAL_TOSSES))
+for i in range(70, TOTAL_TOSSES):
+    probSum -= (nCr(TOTAL_TOSSES, i) * ((1/2)**TOTAL_TOSSES))
+
+print(probSum)
