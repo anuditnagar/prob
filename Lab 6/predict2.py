212a1b8164099cc9f0d13ad6b1fca810b3521464
Anudit Nagar <nagaranudit@gmail.com>
2019-09-30 09:53:16 +0530

    Lab 6 Update

--- Lab 6/predict2.py
+++ Lab 6/predict2.py
@@ -7,41 +7,30 @@ with open('BAJFINANCE-EQ.csv') as csvfile:
     reader = csv.reader(csvfile)
     headers = next(reader)
     for row in reader:
-        # open high low close
         dat = [float(row[1]), float(row[2]), float(row[3]), float(row[4])]
         data.append(dat)
 
 STARTAMOUNT = 10000
-ANALYSECNT = 10
-BETAMOUNT = 50
 
-for ind in range(ANALYSECNT, len(data)):
+originalOpen = data[0][0]
+originalHigh = data[0][1]
+originalLow = data[0][2]
+originalClose = data[0][3]
 
-    up, down, bet, result   = 0, 0, 0, 0
-    for i in range(ind-ANALYSECNT, ind-1):
-        if data[i][3] < data[i][3]:
-            up+=1
-        else:
-            down+=1
+for ind in range(len(data)):
+    predictedClose = (( originalHigh + originalLow ) / 2)
 
-    if(up >= down):
-        # bet on up
-        bet = 0
+    actualClose = data[ind][3]
+    diffClose = (predictedClose - actualClose)
+
+    if (predictedClose > actualClose):
+        STARTAMOUNT += 50
     else:
-        # bet on down
-        bet = 1
+        STARTAMOUNT -= 50
 
-    result = 0
-    if (data[ind][3] > data[ind-1][3]):
-        # market went down
-        result = 0
-    else:
-        # market went up
-        result = 1
-
-    if(result!=bet):
-        STARTAMOUNT -= BETAMOUNT
-    else:
-        STARTAMOUNT -= BETAMOUNT
+    originalOpen = data[ind][0]
+    originalHigh = data[ind][1]
+    originalLow = data[ind][2]
+    originalClose = data[ind][3]
 
 print(STARTAMOUNT)
